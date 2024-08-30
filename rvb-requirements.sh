c60f2664f5ba45e706ff8ae30e8a8e1a57ff9d02
Ben Luo <ben.bb.luo@gmail.com>
2024-08-30 13:14:56 +0930

    Updated requirements

--- rvb-requirements.sh
+++ rvb-requirements.sh
@@ -1,4 +1,4 @@
-pip install pandas[performance,plot,output_formatting,computation] scikit-learn umap-learn glasbey seaborn networkx ipykernel colorcet xgboost polars[all] swifter[notebook,groupby] jupytext
+pip install pandas[performance,plot,output_formatting,computation] scikit-learn umap-learn glasbey seaborn networkx ipykernel colorcet xgboost polars[all] swifter[notebook,groupby] jupytext shap pydotplus black
 pip install --index-url https://download.pytorch.org/whl/cu118 torch torchvision torchaudio
 pip install torch_geometric torcheval torchmetrics tabulate
 pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.1.0+cu118.html
